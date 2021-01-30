4be99f5a2dfd741ba13ab8419c064f0764fce5d8
zachj555 <zachj555@users.noreply.github.com>
2021-01-30 15:38:39 -0800

    Update LibBlock.hs
    Changed higher order functions to be commented out, fixed improper ints to Ints

--- LibBlock.hs
+++ LibBlock.hs
@@ -1,43 +1,48 @@
 
 module LibBlock where
 
-swapIP :: [int] -> [int]
+swapIP :: [Int] -> [Int]
 swapIP x = x !! 2 : x !! 6 : x !! 3 : x !! 1 : x !! 4 : x !! 8 : x !! 5 : x !! 7 : []
 
-swapIPINVERSE ::[int] -> [int]
+swapIPINVERSE ::[Int] -> [Int]
 swapIPINVERSE x = x !! 4 : x !! 1 : x !! 3 : x !! 5 : x !! 7 : x !! 2 : x !! 8 : x !! 6 : []
 
-swapPTEN :: [int] -> [int]
+swapPTEN :: [Int] -> [Int]
 swapPTEN x = x !! 3 : x !! 5 : x !! 2 : x !! 7 : x !! 4 : x !! 10 : x !! 1 : x !! 9 : x !! 8 : x !! 6 : []
 
-swapPEIGHT :: [int] -> [int]
+swapPEIGHT :: [Int] -> [Int]
 swapPEIGHT x = x !! 6 : x !! 3 : x !! 7 : x !! 4 : x !! 8 : x !! 5 : x !! 10 : x !! 9 : []
 --- might get mad because it is going to have a length 10 list and return a length 8 list?
 
-swapEP :: [int] -> [int]
+swapEP :: [Int] -> [Int]
 swapEP x = x !! 4 : x !! 1 : x !! 2 : x !! 3 :x !! 2 : x !! 3 : x !! 4 : x !! 1 : []
 ---- might also use circular shift?
 
-swapPFOUR :: [int] -> [int]
+swapPFOUR :: [Int] -> [Int]
 swapPFOUR x = x !! 2 : x !! 4 : x !! 3 : x !! 1 : []
 
-swapFORFK :: [int] -> [int]
+swapFORFK :: [Int] -> [Int]
 swapFORFK x = x !! 4 : x !! 5 : x !! 6 : x !! 7 : x !! 0 : x !! 1 : x !! 2 : x !! 3 : []
 
-sZERO :: [[int]]
+sZERO :: [[Int]]
 sZERO = [[1,0,3,2],[3,2,1,0],[0,2,1,3],[3,1,3,2]]
 
-sONE :: [[int]]
+sONE :: [[Int]]
 sONE = [[0,1,2,3],[2,0,1,3],[3,0,1,0],[2,1,0,3]]
 
-fK :: [int] -> [int]
-fK x = do
-   let xs = splitAt 4 x
-   let leftHalf = fst xs
-   let rightHalf = snd xs
-   let rightHalfModified = fInner (snd xs)
-   let leftHalfModified = leftHalf xor rightHalfModified
-   x = leftHalfModified ++ rightHalf
+---keyGeneration :: [Int] -> ([Int], [Int])
+---keyGeneration x = do
+---   let keyInitial = swapPTEN x
+
+---fK :: [Int] -> [Int] -> [Int]
+---fK x key = do
+---   let xs = splitAt 4 x
+---   let leftHalf = fst xs
+---   let rightHalf = snd xs
+---   let rightHalfModified = fInner (snd xs) key
+---   let leftHalfModified = leftHalf xor rightHalfModified
+---   let y = leftHalfModified ++ rightHalf
+---   return y
 
 --- bound == list original size
 --- normally R shift would be length - 1 EXCEPT new list is + 1 element, + 1 - 1 negate eachother
