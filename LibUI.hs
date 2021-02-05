f294d1c34fac65ee862d2f2d80a31acb77706878
alexmcmil <77553190+alexmcmil@users.noreply.github.com>
2021-02-05 04:04:02 -0800

    Add files via upload

---
+++ LibUI.hs
@@ -0,0 +1,13 @@
+modlue LibUI where
+
+import Text.Read as R
+import Data.List as L
+
+formatInput :: [Char] -> [Int]
+formatInput x = L.map (R.read . (:"")) x :: [Int]
+
+filterInput :: [Char] -> [Char]
+filterInput x = removeChar ' ' x
+
+removeChar :: Char -> [Char] -> [Char]
+removeChar character string = L.filter (\c -> c/=character) string
