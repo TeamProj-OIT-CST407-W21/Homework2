0e7583846cfc0018ba67d06215a945475d8e7a74
zachj555 <zachj555@users.noreply.github.com>
2021-02-05 15:53:46 -0800

    Commented out unused functions
    Commented out filterInput and removeChar functions, as they were not called

--- LibUI.hs
+++ LibUI.hs
@@ -6,11 +6,11 @@ import Data.List as L
 formatInput :: [Char] -> [Int]
 formatInput x = L.map (R.read . (:"")) x :: [Int]
 
-filterInput :: [Char] -> [Char]
-filterInput x = removeChar ' ' x
+--filterInput :: [Char] -> [Char]
+--filterInput x = removeChar ' ' x
 
-removeChar :: Char -> [Char] -> [Char]
-removeChar character string = L.filter (\c -> c/=character) string
+--removeChar :: Char -> [Char] -> [Char]
+--removeChar character string = L.filter (\c -> c/=character) string
 
 filterLessThanZero :: [Int] -> Int
 filterLessThanZero list = L.length(L.filter (-1<) list)
