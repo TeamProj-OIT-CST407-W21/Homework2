323b364a0d27235956a9aaef1ca906d9bb5b5edb
zachj555 <zachj555@users.noreply.github.com>
2021-02-05 15:09:24 -0800

    finished main, added checks on user input
    This was a pain in the ass.

--- LibUI.hs
+++ LibUI.hs
@@ -11,3 +11,9 @@ filterInput x = removeChar ' ' x
 
 removeChar :: Char -> [Char] -> [Char]
 removeChar character string = L.filter (\c -> c/=character) string
+
+filterLessThanZero :: [Int] -> Int
+filterLessThanZero list = L.length(L.filter (-1<) list)
+
+filterGreaterThanOne :: [Int] -> Int
+filterGreaterThanOne list = L.length(L.filter (<2) list)
--- Main.hs
+++ Main.hs
@@ -1,33 +1,51 @@
 
 import LibBlock
+import LibUI
+import Data.List as L
 import System.Exit
 
 main :: IO()
 main = do
    putStrLn "Please input a 10-bit key: "
    initialKey <- getLine
-   putStrLn "Please input an 8-bit message: "
-   initialInput <- getLine
-   let keyOne = generateKey1 (formatInput (initialKey))
-   let keyTwo = generateKey2 (formatInput (initialKey))
-   let inputFinal = (formatInput (initialInput))
-   putStrLn "Your keys are Key One: "
-   print keyOne
-   putStrLn "\nKey Two: "
-   print keyTwo
-   putStrLn "\nWould you like to Encrypt('e') or Decrypt('d') or Quit ('q'): "
-   choice <- getLine
-   putStrLn "Your choice was: "
-   print choice
-   if (choice == "q")
-      then exitSuccess
----else if (choice == "e")
-------then print "encrypt"
----else if (choice == "d")
-------then print "decrypt"
-   else
-      die "You f**ked up"
----fK initialInput keyOne KeyTwo choice
-
-formatInput :: [Char] -> [Int]
-formatInput x = map (read . (:"")) x :: [Int]
+   if ((L.length (formatInput(initialKey))) /= 10)
+      then die "Improper key length, killing program"
+   else do
+      if ((filterLessThanZero (formatInput(initialKey))) /= (L.length (formatInput(initialKey))))
+         then die "Improper key, value less than zero"
+      else if ((filterGreaterThanOne (formatInput(initialKey))) /= (L.length (formatInput(initialKey))))
+         then die "Improper key, value greater than one"
+      else do
+         putStrLn "Please input an 8-bit message: "
+         initialInput <- getLine
+         let inputFinal = (formatInput (initialInput))
+         if (L.length inputFinal /= 8)
+            then die "Improper message length, killing program"
+         else do
+            if ((filterLessThanZero inputFinal) /= (L.length(inputFinal)))
+               then die "Improper message, value less than zero"
+            else if ((filterGreaterThanOne inputFinal) /= (L.length(inputFinal)))
+               then die "Improper message, value greater than one"
+            else do
+               let keyOne = generateKey1 (formatInput (initialKey))
+               let keyTwo = generateKey2 (formatInput (initialKey))
+               putStrLn "Your keys are Key One: "
+               print keyOne
+               putStrLn "\nKey Two: "
+               print keyTwo
+               putStrLn "\nWould you like to Encrypt('e') or Decrypt('d') or Quit ('q'): "
+               choice <- getLine
+               putStrLn "Your choice was: "
+               print choice
+               if (choice == "q")
+                  then exitSuccess
+               else if (choice == "e")
+                  then do
+                     putStrLn ("Your encrypted message is: ")
+                     print (fkComplete inputFinal keyOne keyTwo)
+               else if (choice == "d")
+                  then do
+                     putStrLn ("Your decrypted message is: ")
+                     print (fkComplete inputFinal keyTwo keyOne)
+               else
+                  die "You f**ked up"
